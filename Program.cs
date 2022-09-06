7bae251995ae0a2a4145af074d63c9766b70791c
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-06 14:04:27 -0300

    Adiciona solução do Desafio email

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+    string nomeCompleto, primeiroNome, segundoNome, dominio = "@fatec.sp.gov.br";
+    int pos1, pos2;
+
+    Console.Write("Digite seu nome completo: ");
+    nomeCompleto = Console.ReadLine()
+    .Replace("das","").Replace("dos","")
+    .Replace("da","").Replace("do","")
+    .Replace("'","").Replace("de","")
+    .Replace("á","a").Replace("â","a")
+    .Replace("é","e").Replace("ê","e")
+    .Replace("í","i").Replace("î","i")
+    .Replace("ó","o").Replace("ô","o")
+    .Replace("ú","u").Replace("û","u");
+    
+pos1 = nomeCompleto.IndexOf(" ");
+
+pos2 = nomeCompleto.LastIndexOf(" ");
+
+primeiroNome = nomeCompleto.Substring(0,pos1);
+
+segundoNome = nomeCompleto.Substring(pos2);
+
+Console.WriteLine("E-mail institucional: " + primeiroNome.Trim() + "."+ segundoNome.Trim().ToLower() + dominio);
